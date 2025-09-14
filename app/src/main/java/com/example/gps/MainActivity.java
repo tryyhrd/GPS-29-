d1fe412bbb7e595093ea357c7c31e8ad941b3137
admin <admin@DESKTOP-E07VVT6>
2025-09-14 18:18:03 +0500

    Прослушиватель местоположения пользователя.Ожгибесов

--- app/src/main/java/com/example/gps/MainActivity.java
+++ app/src/main/java/com/example/gps/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.gps;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.mapview.MapView;
+import com.yandex.runtime.image.ImageProvider;
+
 public class MainActivity extends AppCompatActivity {
+
+    LocationManager _locationManager;
+    TextView textAddress;
+    MapView mapView;
+    LocationListener _locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            if (location != null) {
+                mapView.getMap().move(
+                        new CameraPosition(
+                                new Point(location.getLatitude(), location.getLongitude()), 15, 0, 0
+                        )
+                );
+                mapView.getMap().getMapObjects().clear();
+                mapView.getMap().getMapObjects().addPlacemark(
+                        new Point(location.getLatitude(), location.getLongitude()),
+                        ImageProvider.fromResource(MainActivity.this, R.drawable.location)
+                );
+
+                GetAddressByGPS getAddressByGPS = new GetAddressByGPS(
+                        String.valueOf(location.getLongitude() + ", " + location.getLatitude()),
+                        textAddress
+                );
+                GetAddressByGPS.execute();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
